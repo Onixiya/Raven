b1534ef15284ac5d9e91796b76371ae8dbf5666f
silentstorm <no@no.com>
2024-02-21 19:26:18 +1100

    general update

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,7 +1,7 @@
 namespace Raven{
     public static class ModHelperData{
-        public const string WorksOnVersion="34.3";
-        public const string Version="1.0.3";
+        public const string WorksOnVersion="41.2";
+        public const string Version="1.0.4";
         public const string Name="Raven";
         public const string Description="Adds the Raven from Starcraft 2 as a magic tower into BTD6";
         public const string RepoOwner = "Onixiya";
