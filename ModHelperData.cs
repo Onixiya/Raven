b8b756c7d1c00b88e82d5dcaff8e9490aed69802
silentstorm <29950414+Onixiya@users.noreply.github.com>
2024-12-20 10:23:45 +1100

    add dependencies to modhelperdata

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,5 +6,6 @@ namespace Raven{
         public const string Description="Adds the Raven from Starcraft 2 as a magic tower into BTD6";
         public const string RepoOwner="Onixiya";
         public const string RepoName="Raven";
+        public const string Dependencies="Onixiya/SC2Expansion";
     }
 }
